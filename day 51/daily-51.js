32d48b9359283469cfd14fe2ebd7ac6310334981
Andrade <rodrigo.andrade@c02s8hg0fvh3.wirecard.lan>
2019-08-28 15:19:45 -0300

    Daily Challenge #51 - Valid Curly Braces

---
+++ day 51/daily-51.js
@@ -0,0 +1,43 @@
+class BracerMatcher {
+
+    constructor(){
+        this.defaultValue = 0;
+        this.emptyStringValue = 0;
+        this.rightBraceValue = 1;
+        this.leftBraceOrUnknowValue = -1;
+    }
+
+    isEmptyString(str) {
+        return str === '';
+    }
+
+    isRightBrace(brace) {
+        return brace == '{';
+    }
+
+    isValid(string) {
+        const finalValue = string
+                            .split("")      
+                            .map(brace => 
+                                this.isRightBrace(brace) ? this.rightBraceValue : 
+                                (this.isEmptyString(brace) ? this.emptyStringValue : this.leftBraceOrUnknowValue))  
+                            .reduce((acumulator, currentValue) => 
+                                acumulator + currentValue, 
+                                this.defaultValue);                    
+                        
+        return finalValue == 0 ? true : false;
+    }
+
+   
+}
+
+// testing 
+const bracerMatcher = new BracerMatcher();
+console.log(bracerMatcher.isValid("{{{}{}}}")); // true
+console.log(bracerMatcher.isValid("{{")); // false
+console.log(bracerMatcher.isValid("{}}"));// false
+console.log(bracerMatcher.isValid("{}"));// true
+console.log(bracerMatcher.isValid("should_not_work"));// false
+console.log(bracerMatcher.isValid("!!should?_not _work  111111")); // false
+console.log(bracerMatcher.isValid("!!shout_not_work{}")); // false
+console.log(bracerMatcher.isValid("")); // true
