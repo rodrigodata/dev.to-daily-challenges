0231696dbd90c27e5fe0bbdb570cf49ee09a8c0c
Rodrigo Andrade <warodrigoandrade@gmail.com>
2019-08-28 22:36:03 -0300

    WIP - Challenge 50

--- day-50/day-50.js
+++ day-50/day-50.js
@@ -1,1 +1,16 @@
-// wip
+/**
+ *
+ * Challenge 50
+ *
+ * Given a phone number of integer length N (1 ≤ N ≤ 10) as
+ * a number string (e.g. 555-555-5555 would be passed as "5555555555"),
+ * return an array of all phone numbers as
+ * number strings that would be considered neighbors of that
+ * phone number (which is any numbers ±1).
+ *
+ * Example:
+ * Someone with the phone number 555-555-5555 has
+ * neighbors 555-555-5554 and 555-555-5556.
+ *
+ * Url: https://dev.to/thepracticaldev/daily-challenge-50-number-neighbor-44jm
+ */
